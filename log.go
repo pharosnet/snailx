9cd9796bfa5ba5463446e27b42c98839fb33f16c
RyougiNevermore <ryougi.nevermore@hotmail.com>
2019-08-04 09:32:29 +0800

    add log

---
+++ log.go
@@ -0,0 +1,8 @@
+package snailx
+
+type Logger interface {
+	Infof(format string, args ...interface{})
+	Warningf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatalf(format string, args ...interface{})
+}
